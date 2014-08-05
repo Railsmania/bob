5e328cc18570ea96ef968d4fc55f371f53afefa1
Jordi Polo <mumismo@gmail.com>
2014-08-05 12:10:49 +0200

    see static pages in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # We want to have this as true so we can check our test page in the cloud
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
